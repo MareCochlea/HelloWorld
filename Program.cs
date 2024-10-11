b245fb20b4c46a1acba5a63c48b1fe81bf44ba0a
Maris <marecochlea@gmail.com>
2024-10-11 18:04:40 +0100

    New commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿String name;
+Array scores;
+scores = new int[20];
+
+Console.Title = "My First Program";
 
 Console.WriteLine("Who are you???");
-
 
 // The command below gets your name and sends it to the console.
 name = Console.ReadLine();
 
-
 Console.WriteLine("Oh, hello "+name+"!");
 
 Console.ReadLine();
+
+Console.WriteLine(scores);
+
+Console.WriteLine("Press any key when you're ready to begin.");
+Console.ReadKey(true);
